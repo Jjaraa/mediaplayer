ba7c8be0bcf50261f40492c3fae94db034d0b975
Jonathan <jjara21@ufromail.cl>
2021-09-09 23:42:21 -0300

    se agregan geters y seters a mediaplayer

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -6,9 +6,21 @@ function MediaPlayer(config) {
 }
 
 MediaPlayer.prototype._initPlugins = function () {
-    this.plugins.forEach(plugin => {
-        plugin.run(this);
-    });
+  const player = {
+    play: () => this.play(),
+    pause: () => this.pause(),
+    media: this.media,
+    get muted() {
+      return this.media.muted;
+    },
+    set muted(value) {
+      this.media.muted = value;
+    },
+  };
+
+  this.plugins.forEach((plugin) => {
+    plugin.run(player);
+  });
 };
 
 MediaPlayer.prototype.play = function () {
@@ -27,12 +39,8 @@ MediaPlayer.prototype.togglePlay = function () {
   }
 };
 
-MediaPlayer.prototype.mute = function () {
-    this.media.muted = true;
-}
-
 MediaPlayer.prototype.alterMute = function () {
-    this.media.muted = !this.media.muted;
-}
+  this.media.muted = !this.media.muted;
+};
 
 export default MediaPlayer;
